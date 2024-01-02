e9a2ecba4a51d1030c935342b2449f76229a965c
tinaarobot <146160310+tinaarobot@users.noreply.github.com>
2024-01-02 12:40:19 +0530

    Update group.py

--- DAXXMUSIC/plugins/tools/group.py
+++ DAXXMUSIC/plugins/tools/group.py
@@ -14,11 +14,11 @@ async def brah2(_, msg):
 # invite members on vc
 @app.on_message(filters.video_chat_members_invited)
 async def brah3(app :app, message:Message):
-           text = f"{message.from_user.mention} ɪɴᴠɪᴛᴇᴅ "
+           text = f"๏ {message.from_user.mention} ɪɴᴠɪᴛᴇᴅ "
            x = 0
            for user in message.video_chat_members_invited.users:
              try:
-               text += f"๏ [{user.first_name}](tg://user?id={user.id}) "
+               text += f" [{user.first_name}](tg://user?id={user.id}) "
                x += 1
              except Exception:
                pass
